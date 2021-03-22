535f26a8213cdaebc7e4ac01832515d8c3db4667
Elia Mercatanti <eliamercatanti@gmail.com>
2021-03-22 17:43:04 +0100

    First test case for guest controller

---
+++ src/test/java/com/eliamercatanti/guesthousebooking/controller/GuestControllerTest.java
@@ -0,0 +1,14 @@
+package com.eliamercatanti.guesthousebooking.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GuestControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
