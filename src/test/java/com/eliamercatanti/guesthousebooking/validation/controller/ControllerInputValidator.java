8f075a31a92855ebcd6de89dc4c327b4a67fb629
Elia Mercatanti <eliamercatanti@gmail.com>
2021-04-16 17:07:15 +0200

    New test testValidateEmailShouldReturnFalseWhenNullString()
    New test for Controller Input Validator

---
+++ src/test/java/com/eliamercatanti/guesthousebooking/validation/controller/ControllerInputValidator.java
@@ -0,0 +1,31 @@
+package com.eliamercatanti.guesthousebooking.validation.controller;
+
+import java.time.LocalDate;
+
+import com.eliamercatanti.guesthousebooking.validation.InputValidation;
+
+public class ControllerInputValidator implements InputValidation {
+
+	@Override
+	public boolean validateEmail(String email) {
+		if (email == null) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
+
+	@Override
+	public boolean validateTelephoneNumber(String telephoneNumber) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public LocalDate validateDate(String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/test/java/com/eliamercatanti/guesthousebooking/validation/controller/ControllerInputValidatorTest.java
@@ -0,0 +1,38 @@
+package com.eliamercatanti.guesthousebooking.validation.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Tests for Controller Input Validator")
+class ControllerInputValidatorTest {
+
+	static ControllerInputValidator controllerInputValidator;
+	
+	@BeforeAll
+	static void setup() {
+		controllerInputValidator = new ControllerInputValidator();
+	}
+
+	@Nested
+	@DisplayName("Controller Input Validator Happy Cases")
+	class HappyCases {
+		
+	}
+
+	@Nested
+	@DisplayName("Controller Input Validator Exceptional Cases")
+	class ExceptionalCases {
+		
+		@Test
+		@DisplayName("Email validation should return false when string is null - testValidateEmailShouldReturnFalseWhenStringIsNull()")
+		void testValidateEmailShouldReturnFalseWhenStringIsNull() {
+			assertThat(controllerInputValidator.validateEmail(null)).isFalse();
+		}
+
+	}
+
+}
