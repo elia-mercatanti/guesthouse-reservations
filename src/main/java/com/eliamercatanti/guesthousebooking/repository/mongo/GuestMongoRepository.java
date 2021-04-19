b38aa388ae9c2d80c04d04defb77860adb2938e8
Elia Mercatanti <eliamercatanti@gmail.com>
2021-04-19 17:32:35 +0200

    Implementation for testFindAllShouldReturnAnEmptyListWhenDatabaseIsEmpty()

--- src/main/java/com/eliamercatanti/guesthousebooking/repository/mongo/GuestMongoRepository.java
+++ src/main/java/com/eliamercatanti/guesthousebooking/repository/mongo/GuestMongoRepository.java
@@ -1,21 +1,41 @@
 package com.eliamercatanti.guesthousebooking.repository.mongo;
 
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
 import com.eliamercatanti.guesthousebooking.model.Guest;
 import com.eliamercatanti.guesthousebooking.repository.GuestRepository;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoCollection;
 
 public class GuestMongoRepository implements GuestRepository {
 
-	public GuestMongoRepository(MongoClient client, String databaseName, String collectionName) {
-		// TODO Auto-generated constructor stub
+	private MongoCollection<Guest> guestCollection;
+
+	public GuestMongoRepository(MongoClient mongoClient, String databaseName, String collectionName) {
+		CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+				fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+		guestCollection = mongoClient.getDatabase(databaseName).withCodecRegistry(pojoCodecRegistry)
+				.getCollection(collectionName, Guest.class);
 	}
 
 	@Override
 	public List<Guest> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		if (guestCollection.countDocuments() == 0) {
+			return Collections.emptyList();
+		}
+		else {
+			return new ArrayList<>();
+		}
+		
 	}
 
 	@Override
