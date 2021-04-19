4680bc4afde5bc85cd21f4a0f4174f1aaadd206b
Elia Mercatanti <eliamercatanti@gmail.com>
2021-04-19 22:24:04 +0200

    Code Implementation for testFindAllShouldReturnAnEmptyListWhenDatabaseIsEmpty()

--- src/main/java/com/eliamercatanti/guesthousebooking/repository/mongo/BookingMongoRepository.java
+++ src/main/java/com/eliamercatanti/guesthousebooking/repository/mongo/BookingMongoRepository.java
@@ -1,23 +1,41 @@
 package com.eliamercatanti.guesthousebooking.repository.mongo;
 
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
 import com.eliamercatanti.guesthousebooking.model.Booking;
 import com.eliamercatanti.guesthousebooking.model.Room;
 import com.eliamercatanti.guesthousebooking.repository.BookingRepository;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class BookingMongoRepository implements BookingRepository {
 
+	private MongoCollection<Booking> bookingCollection;
+
 	public BookingMongoRepository(MongoClient mongoClient, String databaseName, String collectionName) {
-		// TODO Auto-generated constructor stub
+		CodecRegistry pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+				fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+		MongoDatabase database = mongoClient.getDatabase(databaseName).withCodecRegistry(pojoCodecRegistry);
+		bookingCollection = database.getCollection(collectionName, Booking.class);
 	}
 
 	@Override
 	public List<Booking> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		if (bookingCollection.countDocuments() == 0) {
+			return Collections.emptyList();
+		}
+		return new ArrayList<>();
 	}
 
 	@Override
