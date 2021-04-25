90dd007b6f2e40f3b3416448cab4fb4353db6b5e
Elia Mercatanti <eliamercatanti@gmail.com>
2021-04-25 18:20:46 +0200

    New IT testFindByRoom() for Booking Mongo Repository

--- src/it/java/com/eliamercatanti/guesthousebooking/repository/mongo/BookingMongoRepositoryIT.java
+++ src/it/java/com/eliamercatanti/guesthousebooking/repository/mongo/BookingMongoRepositoryIT.java
@@ -130,6 +130,19 @@ class BookingMongoRepositoryIT {
 		assertThat(bookingsList).containsExactly(booking1, booking2, booking3);
 	}
 
+	@Test
+	@DisplayName("Find all bookings for a given room - testFindByRoom()")
+	void testFindByRoom() {
+		Booking booking1 = new Booking(new ObjectId().toString(), LocalDate.of(2021, 1, 1), LocalDate.of(2021, 1, 10),
+				1, Room.SINGLE);
+		Booking booking2 = new Booking(new ObjectId().toString(), LocalDate.of(2021, 1, 10), LocalDate.of(2021, 1, 20),
+				2, Room.DOUBLE);
+		Booking booking3 = new Booking(new ObjectId().toString(), LocalDate.of(2021, 1, 20), LocalDate.of(2021, 1, 30),
+				1, Room.SINGLE);
+		bookingCollection.insertMany(Arrays.asList(booking1, booking2, booking3));
+		assertThat(bookingMongoRepository.findByRoom(Room.SINGLE)).containsExactly(booking1, booking3);
+	}
+
 	private List<Booking> getBookingsList() {
 		return StreamSupport.stream(bookingCollection.find().spliterator(), false).collect(Collectors.toList());
 	}
